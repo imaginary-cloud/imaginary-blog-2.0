e3279cb64ba3127d76c78b08e8f038619cbad0ce
Fábio Carvalho <fcarvalho@imaginarycloud.com>
2023-12-15 16:55:44 +0000

    Add Card component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'staging.imaginarycloud.com',
+        port: '',
+        pathname: '/blog/content/images/**',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,28 +1,53 @@
-import { PostOrPage } from '@tryghost/content-api';
+import Image from 'next/image';
 import Link from 'next/link';
+import { PostOrPage } from '@tryghost/content-api';
+
+import {
+  Card as Cardx,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { truncateString } from '@/lib/utils';
 
 type CardData = {
   data: PostOrPage;
 };
 
 export default function Card({ data }: CardData) {
-  const { slug, title, excerpt, authors } = data;
+  const { slug, title, excerpt, authors, feature_image } = data;
 
   return (
     <Link href={`/post/${slug}`} className="flex-1">
-      <div className="flex flex-col bg-white p-4 rounded-md shadow-md min-h-full">
-        <h2 className="text-xl text-blue-500 font-bold mb-2">{title}</h2>
-        <p className="text-gray-700">{excerpt}</p>
-        {authors?.length && (
-          <div className="mt-auto">
-            {authors.map(({ id, name }) => (
-              <p key={id} className="text-gray-400 font-light mt-5">
-                Author: {name}
-              </p>
-            ))}
-          </div>
-        )}
-      </div>
+      <Cardx className="flex flex-col min-h-full">
+        <CardHeader>
+          {feature_image && (
+            <CardDescription>
+              <Image
+                src={feature_image}
+                alt={`img-${slug}`}
+                width={300}
+                height={300}
+              />
+            </CardDescription>
+          )}
+          <CardTitle>{title}</CardTitle>
+        </CardHeader>
+        {excerpt && <CardContent>{truncateString(excerpt)}</CardContent>}
+        <CardFooter className="mt-auto">
+          {authors?.length && (
+            <div className="mt-auto">
+              {authors.map(({ id, name }) => (
+                <span key={id} className="text-gray-400 font-light mt-5">
+                  Author: {name}
+                </span>
+              ))}
+            </div>
+          )}
+        </CardFooter>
+      </Cardx>
     </Link>
   );
 }
