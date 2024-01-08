e0e1354f818c70093e11eb90975f26e1f50bbef8
Fábio Carvalho <fcarvalho@imaginarycloud.com>
2024-01-08 15:05:10 +0000

    Fix styles of accordion and sheet  shadcn-ui components

--- src/components/MenuMobile.tsx
+++ src/components/MenuMobile.tsx
@@ -21,65 +21,71 @@ export default function MenuMobile() {
           <MenuIcon />
         </SheetTrigger>
         <SheetContent>
-          {menus.map(({ title, sublinks, url }, index) => {
-            const groupedSublinks = (sublinks: any) =>
-              groupSublinksByParent(sublinks);
-            return (
-              <>
-                {sublinks ? (
-                  <Accordion type="single" collapsible key={index}>
-                    <AccordionItem key={title} value={title}>
-                      <AccordionTrigger>{title}</AccordionTrigger>
-                      <AccordionContent>
-                        {sublinks && (
-                          <>
-                            {groupedSublinks(sublinks)['default']?.map((sublink) => (
-                              <a
-                                key={sublink.title}
-                                href={sublink.url}
-                                onClick={(e) => {
-                                  e.preventDefault();
-                                  router.push(sublink.url);
-                                }}
-                              >
-                                {sublink.title}
-                              </a>
-                            ))}
+          <div className="flex flex-col gap-4 mt-10 px-0">
+            {menus.map(({ title, sublinks, url }, index) => {
+              const groupedSublinks = (sublinks: any) =>
+                groupSublinksByParent(sublinks);
+              return (
+                <>
+                  {sublinks ? (
+                    <Accordion type="single" collapsible key={index}>
+                      <AccordionItem key={title} value={title}>
+                        <AccordionTrigger>{title}</AccordionTrigger>
+                        <AccordionContent className="flex flex-col justify-start gap-3">
+                          {sublinks && (
+                            <>
+                              {groupedSublinks(sublinks)['default']?.map(
+                                (sublink) => (
+                                  <a
+                                    key={sublink.title}
+                                    href={sublink.url}
+                                    onClick={(e) => {
+                                      e.preventDefault();
+                                      router.push(sublink.url);
+                                    }}
+                                  >
+                                    {sublink.title}
+                                  </a>
+                                )
+                              )}
 
-                            {Object.entries(groupedSublinks(sublinks))
-                              .filter(([parent]) => parent !== 'default')
-                              .map(([parent, links]) => (
-                                <Accordion key={parent} type="single" collapsible>
-                                  <AccordionItem value={parent}>
-                                    <AccordionTrigger>{parent}</AccordionTrigger>
-                                    <AccordionContent>
-                                      {links.map((link) => (
-                                        <a
-                                          key={link.title}
-                                          href={link.url}
-                                          onClick={(e) => {
-                                            e.preventDefault();
-                                            router.push(link.url);
-                                          }}
-                                        >
-                                          {link.title}
-                                        </a>
-                                      ))}
-                                    </AccordionContent>
-                                  </AccordionItem>
-                                </Accordion>
-                              ))}
-                          </>
-                        )}
-                      </AccordionContent>
-                    </AccordionItem>
-                  </Accordion>
-                ) : (
-                  <a>{title}</a>
-                )}
-              </>
-            );
-          })}
+                              {Object.entries(groupedSublinks(sublinks))
+                                .filter(([parent]) => parent !== 'default')
+                                .map(([parent, links]) => (
+                                  <Accordion key={parent} type="single" collapsible>
+                                    <AccordionItem value={parent}>
+                                      <AccordionTrigger>{parent}</AccordionTrigger>
+                                      <AccordionContent className="flex flex-col justify-start gap-2">
+                                        <div className="flex flex-col mt-3 gap-2">
+                                          {links.map((link) => (
+                                            <a
+                                              key={link.title}
+                                              href={link.url}
+                                              onClick={(e) => {
+                                                e.preventDefault();
+                                                router.push(link.url);
+                                              }}
+                                            >
+                                              {link.title}
+                                            </a>
+                                          ))}
+                                        </div>
+                                      </AccordionContent>
+                                    </AccordionItem>
+                                  </Accordion>
+                                ))}
+                            </>
+                          )}
+                        </AccordionContent>
+                      </AccordionItem>
+                    </Accordion>
+                  ) : (
+                    <a>{title}</a>
+                  )}
+                </>
+              );
+            })}
+          </div>
         </SheetContent>
       </Sheet>
     </div>
