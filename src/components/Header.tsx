1036f06f5044b497b24b7ee6c215f47a1a0b83fd
Fábio Carvalho <fcarvalho@imaginarycloud.com>
2024-01-08 15:07:09 +0000

    Remove OldNavbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,8 @@
 import Navbar from './Navbar';
-import OldNavbar from './OldNavbar';
 
 export default function Header() {
   return (
     <header>
-      {/* <OldNavbar /> */}
       <Navbar />
     </header>
   );
