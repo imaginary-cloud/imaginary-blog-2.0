2be0e6e6839824dfcd18c8bc2ec908be36c95133
Fábio Carvalho <fcarvalho@imaginarycloud.com>
2024-01-09 11:17:06 +0000

    Add styles to hide sheet on screen resize

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -23,7 +23,7 @@ const SheetOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SheetPrimitive.Overlay
     className={cn(
-      'fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+      'lg:hidden fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
       className
     )}
     {...props}
@@ -63,7 +63,7 @@ const SheetContent = React.forwardRef<
     <SheetOverlay />
     <SheetPrimitive.Content
       ref={ref}
-      className={cn('overflow-scroll', sheetVariants({ side }), className)}
+      className={cn('lg:hidden overflow-scroll', sheetVariants({ side }), className)}
       {...props}
     >
       {children}
